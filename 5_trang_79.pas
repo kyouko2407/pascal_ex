bcea63a2ce4727404a3924f4e7b5d0d9947d0d3c
Tăng Quang Dũng <95020999+lemoon05@users.noreply.github.com>
2022-03-10 16:08:05 +0700

    Add files via upload

---
+++ 5_trang_79.pas
@@ -0,0 +1,26 @@
+//Đưa ra True nếu dãy số trong file in.txt là cấp số cộng và ngược lại
+var i,n,d: word;
+    inp, oup: text;
+    a: array[1..100] of integer;
+    t,f :string;
+begin
+    assign(inp,'in.txt');reset(inp);
+    assign(oup,'out.txt');rewrite(oup);
+    readln(inp,n);
+    t := 'True';
+    f := 'False';
+    for i:= 1 to n do
+        read(inp,a[i]);
+    d := a[2] - a[1];
+    for i:=2 to n do
+        
+            if (a[i] - a[i-1] <> d) then 
+                begin;
+                    write(oup,f);
+                    break;
+                end;
+        if i = n then write(oup,t);
+    close(inp);close(oup);
+    readln
+end.
+
