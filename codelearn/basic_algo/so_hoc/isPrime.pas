85eae733e1d1a8069aa8a9453623b7e5a1b5c98e
Tăng Quang Dũng <95020999+lemoon05@users.noreply.github.com>
2022-03-11 10:41:06 +0700

    Add files via upload

---
+++ codelearn/basic_algo/so_hoc/isPrime.pas
@@ -0,0 +1,17 @@
+{
+Đề bài.
+Một số nguyên tố là một số tự nhiên lớn hơn 1 và không thể tạo thành từ tích của hai số tự nhiên nhỏ hơn.
+
+Ví dụ số 2, 3, 5 được gọi là số nguyên tố
+
+Viết một hàm xác định xem một số nguyên dương đã cho có phải là số nguyên tố hay không.
+}
+var count, n,i: integer;
+begin
+    count := 0;
+    write('n: ');readln(n);
+    for i:= 1 to n do 
+        if (n mod i = 0) then count := count + 1;
+    if count = 2 then writeln('True') else writeln('False');
+    readln
+end.
