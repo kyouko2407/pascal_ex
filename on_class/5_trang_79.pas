d39a30ac675bf2f27f30f4516db03f805ca6f071
lemoon05 <95020999+lemoon05@users.noreply.github.com>
2022-03-16 00:15:32 +0700

    delete t f

--- on_class/5_trang_79.pas
+++ on_class/5_trang_79.pas
@@ -2,13 +2,12 @@
 var i,n,d: word;
     inp, oup: text;
     a: array[1..100] of integer;
-    t,f :string;
+    
 begin
     assign(inp,'in.txt');reset(inp);
     assign(oup,'out.txt');rewrite(oup);
     readln(inp,n);
-    t := 'True';
-    f := 'False';
+    
     for i:= 1 to n do
         read(inp,a[i]);
     d := a[2] - a[1];
@@ -16,10 +15,10 @@ begin
         
             if (a[i] - a[i-1] <> d) then 
                 begin;
-                    write(oup,f);
+                    write(oup,'False');
                     break;
                 end;
-        if i = n then write(oup,t);
+        if i = n then write(oup,'True');
     close(inp);close(oup);
     readln
 end.
