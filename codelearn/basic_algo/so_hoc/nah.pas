057abe0792eb84f868fea767fe7b280a11345782
lemoon05 <95020999+lemoon05@users.noreply.github.com>
2022-03-16 09:20:50 +0700

    .

---
+++ codelearn/basic_algo/so_hoc/nah.pas
@@ -0,0 +1,17 @@
+Var s: string; 
+    i: byte;
+    inp, oup: text; 
+Begin 
+    assign(inp,'xau.txt');reset(inp);
+    assign(oup,'xau1.txt');rewrite(oup);
+    readln(inp,s);
+    i:=Pos('em',s); 
+    While i<> 0 do 
+    Begin 
+        Delete(s,i,3); 
+        Insert('anh',s,i); 
+        i:=Pos('em',s); 
+    End; 
+    writeln(oup, s); 
+    readln
+End. 
